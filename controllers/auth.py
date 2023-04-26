040be9b50b624451c12f16af663f37033e0fe55a
jason pruski <jasonmilespruski@outlook.com>
2023-04-25 20:56:22 -0400

    debug potential asynchronous problems

--- controllers/auth.py
+++ controllers/auth.py
@@ -40,6 +40,7 @@ async def login(request):
     data = await _login(username, password)
     # this is f---ed up and unsafe!!!!!!!!!!!!!!!!!
     # change to jwt? username & password w/ httponly
+    print(1, data)
     if data:
         session = await get_session(request)
         print('login', data)
@@ -47,4 +48,5 @@ async def login(request):
         print('sessionid', session['id'])
         return web_response.json_response(True)
     else:
+        print(2, data)
         return web_response.json_response(False)
