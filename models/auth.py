d2b768ea2ca3afb53c85e66ce89e7d5a1d711be9
jason pruski <jasonmilespruski@outlook.com>
2023-04-25 22:21:00 -0400

    debug bcrypt issues (12)

--- models/auth.py
+++ models/auth.py
@@ -40,7 +40,7 @@ async def _login(username,password):
         (username,))
     ret = await cur.fetchone()
     conn.close()
-    if ret and bcrypt.checkpw(password.encode('utf8'), ret[1]):
+    if ret and bcrypt.checkpw(password.encode('utf8'), ret[1].encode('utf8')):
         return ret[0]
     else:
         return 0
